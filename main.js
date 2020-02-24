89c435c0c436244e91ef89575af1d366b3726e19
Calo Corp <marc.calo@codeimmersives.com>
2020-02-24 17:34:31 -0500

    complete

--- main.js
+++ main.js
@@ -3,10 +3,44 @@
  ******************/
 
 
+function getFirstItemFrom (array){
 
+  return array[0]
+}
 
+function getLastItemFrom (array){
 
+  return array[array.length-1]
+}
+function getIndex3 (array){
+if (array.length < 4) {
+  return array[array.length-1]
+}
+else{
+  return array[3] 
+ } 
+}
 
+function isLongList (array){
+
+  if (array.length >= 10){
+    return true 
+    
+  }
+  else {return false}
+}
+
+function firstItemIsNumber (array){
+
+if (typeof array[0]=== 'number'){
+  return true 
+}
+else {return false} 
+}
+function secondCharOfThirdString (array){
+
+  return array[2][1]
+}
 
  /********************************************
  * CODE DOWN HERE IS FOR INTERNAL USE ONLY. *
